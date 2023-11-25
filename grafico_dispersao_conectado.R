61e406a23fbea696050c0bc566c4ac3225fbf7de
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2023-11-24 23:06:00 -0300

    Add files via upload

--- grafico_dispersao_conectado.R
+++ grafico_dispersao_conectado.R
@@ -9,5 +9,10 @@
 ### É importante entender que um gráfico de dispersão conectado é basicamente uma
 ### junção entre um gráfico de dispersão e um gráfico de linhas.
 
-### 
+### Existem dois tipos de gráficos de dispersão conectado. O primeiro é simplesmente
+### um gráfico de linhas com pontos adicionados ao topo das linhas. Ele toma como
+### entrada duas variáveis numéricas apenas. O segundo tipo mostra a relação entre
+### duas avriáveis numéricas ao longo do tempo, e requer três variáveis numéricas.
+### Para esse segundo tipo de gráfico, pode ser usado anotações sobre a terceira
+### variável.
 
